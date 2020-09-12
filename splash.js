9444eada0966697dc4ea2842ec46ff462c60f556
Jake Mulley <71042647+Jake-Mulley@users.noreply.github.com>
2020-09-12 20:02:24 +0100

    Update splash.js

--- splash.js
+++ splash.js
@@ -1,8 +1,21 @@
-var splash = document.getElementById("splashScreen");
+let canvas;
+let context;
+let width;
+let height;
+let splash;
+
 
 document.addEventListener('DOMContentLoaded', init, false);
 
 function init(){
+  canvas = document.querySelector('canvas');
+  context = canvas.getContext('2d');
+  width = canvas.width;
+  height = canvas.height;
+  splash  = document.getElementById("splashScreen");
   splash.style.display = "block";
+  context.fillStyle = white;
+  context.fillRect(20, 20, 20, 20);
+  interval_id = window.setInterval(draw, 105);
 }
   
