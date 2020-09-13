474e423708fcb9ce1530a81e4673f71c9a35e064
Jake Mulley <71042647+Jake-Mulley@users.noreply.github.com>
2020-09-13 14:25:21 +0100

    Update splash.js

--- splash.js
+++ splash.js
@@ -8,10 +8,6 @@ let splash;
 document.addEventListener('DOMContentLoaded', init, false);
 
 function init(){
-  canvas = document.querySelector('canvas');
-  context = canvas.getContext('2d');
-  width = canvas.width;
-  height = canvas.height;
   setTimeout(function(){document.getElementById("splashScreen").style.display = "none";}, 3000);
  
 }
