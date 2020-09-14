cc59d9c648e6bd64310596f8e8bd6548901fd7c4
Jake Mulley <71042647+Jake-Mulley@users.noreply.github.com>
2020-09-14 16:00:48 +0100

    Update splash.js

--- splash.js
+++ splash.js
@@ -3,12 +3,21 @@ let context;
 let width;
 let height;
 let splash;
+let nav;
 
 
 document.addEventListener('DOMContentLoaded', init, false);
 
 function init(){
-  setTimeout(function(){document.getElementById("splashScreen").style.display = "none";}, 3000);
- 
+  setTimeout(function(){document.getElementById("splashScreen").style.display = "none";}, 2500);
+  document.getElementById("testBox").addEventListener("click", displayNav());
+  
 }
-
+function displayNav(){
+  nav = document.getElementsByTagName("nav");
+  if (nav.style.display === "none") {
+    nav.style.display = "block"
+  }else{
+    nav.style.display = "none"
+  }
+}
